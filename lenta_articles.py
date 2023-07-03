e722dd6ef3343021d9be3bd33f85381899012a20
Компьютер <vitamin48>
2023-07-03 11:25:40 +0300

    Рабочая версия, если нет детектирования со стороны сайта

--- lenta_articles.py
+++ lenta_articles.py
@@ -52,32 +52,47 @@ class LentaArticles:
         self.__service = Service('chromedriver.exe')
         # self.browser = uc.Chrome(options=self.__options)
         self.browser = webdriver.Chrome(service=self.__service, options=self.__options)
-        # self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-        #     'source': '''
-        # delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
-        # delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
-        # delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
-        #     '''
-        # })
+        """Необходимо добавить, чтобы не было детектирования эмуляции"""
+        self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            'source': '''
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+            '''
+        })
         self.all_art = []
 
     def get_arts_by_catalogs(self, catalogs):
         """Получить список товаров из каталога, проход по всем страницам"""
         for i in range(len(catalogs)):
             page = 1
-            print(f'={bcolors.OKGREEN}{catalogs[i]}{bcolors.ENDC}')
-            while True:
+            print(f'{bcolors.OKGREEN}{catalogs[i][8:]}{bcolors.ENDC}')
+            while page != 0:
                 try:
-                    print(page)
                     self.browser.get(f'{catalogs[i]}?page={page}')
-                    time.sleep(2)
-                    soup = BeautifulSoup(self.browser.page_source, 'lxml')
-                    print()
-                    # products =
+                    print(self.browser.current_url)
+                    time.sleep(1)
+                    if self.browser.current_url != catalogs[i]:
+                        soup = BeautifulSoup(self.browser.page_source, 'lxml')
+                        products = soup.find('div', class_='catalog-grid__grid catalog-grid')
+                        href = products.findAll('a',
+                                                class_='lui-card lui-sku-product-card sku-card-small-container catalog-grid-sku-product-card')
+                        for hr in href:
+                            url = hr.attrs['href']
+                            self.all_art.append(url)
+                        page += 1
+                    else:
+                        page = 0
                 except Exception as exp:
                     print(f'{bcolors.FAIL}ERROR: {exp}{bcolors.ENDC}')
                     time.sleep(10)
                     continue
+        self.write_txt_file_articles()
+
+    def write_txt_file_articles(self):
+        with open('articles.txt', 'a') as output:
+            for row in self.all_art:
+                output.write(str(row) + '\n')
 
 
 def main():
